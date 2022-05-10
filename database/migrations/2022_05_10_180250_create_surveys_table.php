674eb0d3e03bda0d1e3597423da4f5b44b6eec5d
CharaeKeow <charae.ehsin@gmail.com>
2022-05-11 02:27:02 +0800

    create users table & its columns

---
+++ database/migrations/2022_05_10_180250_create_surveys_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSurveysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surveys', function (Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('age_group');
+            $table->integer('education_level');
+            $table->integer('monthly_income');
+            $table->string('gender', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surveys');
+    }
+}
